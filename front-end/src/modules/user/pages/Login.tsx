e694386e5cb262505e884d015109dbf110bffcfd
namanjain2012 <124208288+namanjain2012@users.noreply.github.com>
2025-07-21 11:14:08 +0530

    registration form ui

--- front-end/src/modules/user/pages/Login.tsx
+++ front-end/src/modules/user/pages/Login.tsx
@@ -1,5 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 const Login = ()=>{
-    return (<h1>Login</h1>)
+    return (<Card className="w-full max-w-md mx-auto">
+        <CardHeader>
+            <CardTitle className="space-y-1 text-center">Login Here</CardTitle>
+            <CardDescription className="text-center">Music App Login Form</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <div className="grid w-full max-w-sm items-center gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" placeholder="Password" />
+                <Button>Login</Button>
+                </div>
+        </CardContent>
+    </Card>)
 }
 
 export default Login;
--- front-end/src/modules/user/pages/Register.tsx
+++ front-end/src/modules/user/pages/Register.tsx
@@ -1,8 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 
 const Register = ()=>{
-    return (<div><h1>Register</h1>
-        <Button variant="outline">Register</Button>
-        </div>)
+    return (<Card className="w-full max-w-md mx-auto">
+        <CardHeader>
+            <CardTitle className="space-y-1 text-center">Register Here</CardTitle>
+            <CardDescription className="text-center">Music App Form</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <div className="grid w-full max-w-sm items-center gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" placeholder="Password" />
+                <Label htmlFor="name">Name</Label>
+                <Input type="text" id="name" placeholder="name" />
+                <Button>Register</Button>
+                </div>
+        </CardContent>
+    </Card>)
 }
 export default Register;
