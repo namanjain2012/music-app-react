d8137c7f43db89ada8d5fdf80a38e35488ce76fc
namanjain2012 <124208288+namanjain2012@users.noreply.github.com>
2025-07-24 13:00:21 +0530

    backend API mocking

---
+++ front-end/src/modules/user/api/user-api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = API_BASE_URL;
+
+export const doRegister = (userData:unknown)=>{
+    console.log("API BASE URL : ",API_BASE_URL," User data is : ",userData);
+    return axios.post('users',{data:userData});
+}
--- front-end/src/modules/user/pages/Register.tsx
+++ front-end/src/modules/user/pages/Register.tsx
@@ -1,34 +1,105 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
-import {useForm} from "react-hook-form";
-const Register = ()=>{
-    const {register,handleSubmit,formState:{errors}} = useForm();
-    const registerSubmit = (userData:unknown)=>{
-        console.log("Form Submit : ",userData);
-    }
-    return (<Card className="w-full max-w-md mx-auto">
-        <CardHeader>
-            <CardTitle className="space-y-1 text-center">Register Here</CardTitle>
-            <CardDescription className="text-center">Music App Form</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <form onSubmit={handleSubmit(registerSubmit)}>
-            <div className="grid w-full max-w-sm items-center gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input {...register('email')} type="email" id="email" placeholder="Email" />
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "../validations/register-validation";
+import { doRegister } from "../api/user-api";
+import { useNavigate } from "react-router-dom";
+ 
+const Register = () => {
+	const navigate = useNavigate();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(registerSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+			name: "",
+		},
+	});
+	const registerSubmit = async (userData: unknown) => {
+		console.log("Form Submit : ", userData);
+		try {
+			const result = await doRegister(userData);
+			if(result.data.id){
+        navigate("/login");
+      }
+      else{
+        console.log("Unable to register");
+      }
+      console.log("Result is : ",result);
+		} catch (err) {
+			console.log("Register failed", err);
+		}
+	};
+	return (
+		<Card className="w-full max-w-md mx-auto">
+			<CardHeader>
+				<CardTitle className="space-y-1 text-center">
+					Register Here
+				</CardTitle>
+				<CardDescription className="text-center">
+					Music App Form
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form onSubmit={handleSubmit(registerSubmit)}>
+					<div className="grid w-full max-w-sm items-center gap-3">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							{...register("email")}
+							type="email"
+							id="email"
+							placeholder="Email"
+						/>
+						<span className="text-red-500">
+							{errors.email && errors.email.message}
+						</span>
+					</div>
 
-                <Label htmlFor="password">Password</Label>
-                <Input {...register('password')} type="password" id="password" placeholder="Password" />
+					<div className="grid w-full max-w-sm items-center gap-3">
+						<Label htmlFor="password">Password</Label>
+						<Input
+							{...register("password")}
+							type="password"
+							id="password"
+							placeholder="Password"
+						/>
+						<span className="text-red-500">
+							{errors.password && errors.password.message}
+						</span>
+					</div>
 
-                <Label htmlFor="name">Name</Label>
-                <Input {...register('name')} type="text" id="name" placeholder="name" />
+					<div className="grid w-full max-w-sm items-center gap-3">
+						<Label htmlFor="name">Name</Label>
+						<Input
+							{...register("name")}
+							type="text"
+							id="name"
+							placeholder="name"
+						/>
+						<span className="text-red-500">
+							{errors.name && errors.name.message}
+						</span>
+					</div>
 
-                <Button>Register</Button>
-                </div>
-                </form>
-        </CardContent>
-    </Card>)
-}
+					<div className="grid w-full max-w-sm items-center gap-3">
+						<Button>Register</Button>
+					</div>
+				</form>
+			</CardContent>
+		</Card>
+	);
+};
 export default Register;
