77c20500dbbab669a0b2f9fc2f326707dc4699a0
namanjain2012 <124208288+namanjain2012@users.noreply.github.com>
2025-08-01 01:24:05 +0530

    adding more middlewares

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -2,6 +2,7 @@ export const login = (req,res)=>{
     res.json({message : 'Login'});
 };
 export const register = (req,res)=>{
+    console.log("Data Received",req.body);
     res.json({message : 'Register'});
 };
 export const profile = (req,res)=>{
--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import chalk from 'chalk';
-import userRoutes from './api/v1/routes/user-routes.js';
-import musicRoutes from './api/v1/routes/music-routes.js';
 import { indexRoute } from './api/v1/routes/index.js';
 import { Error404 } from './utils/middlewares/404.js';
-
+import cors from 'cors';
 const app = express();
 console.log("Express type : ",typeof express);
 console.log("App type : ",typeof app);
 
+//MIDDLEWARES
+app.use(cors());
+app.use(express.json());
 app.use('/api/v1',indexRoute);
-
 app.use(Error404);
+
 // app.get('/',(req,res)=>{
 //     res.send('<h1>Hello User</h1>');
 // });
