f1b3911e004b4c3e2d5cf1b1877d7955f55c5136
namanjain2012 <124208288+namanjain2012@users.noreply.github.com>
2025-08-05 02:04:23 +0530

    building db connection

--- back-end/server.js
+++ back-end/server.js
@@ -3,10 +3,13 @@ import chalk from 'chalk';
 import { indexRoute } from './api/v1/routes/index.js';
 import { Error404 } from './utils/middlewares/404.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { createConnection } from './utils/db/connection.js';
 const app = express();
 console.log("Express type : ",typeof express);
 console.log("App type : ",typeof app);
 
+dotenv.config();
 //MIDDLEWARES
 app.use(cors());
 app.use(express.json());
@@ -19,11 +22,16 @@ app.use(Error404);
 // app.get('/login',(req,res)=>{
 //     res.send('<h1>Login...</h1>');
 // });
-const server = app.listen(7777,(err)=>{
-    if(err){
-        console.log(chalk.redBright.bold('Server crash '),err);
-    }
-    else{
-        console.log(chalk.greenBright.bold('Server Up and Running '),server.address().port);
-    }
+const promise = createConnection();
+promise.then(()=>{
+    const server = app.listen(7777,(err)=>{
+        if(err){
+            console.log(chalk.redBright.bold('Server crash '),err);
+        }
+        else{
+            console.log(chalk.greenBright.bold('Server Up and Running '),server.address().port);
+        }
+    })
+}).catch(err=>{
+    console.log(chalk.redBright.bold('DB Crash '),err);
 })
---
+++ back-end/utils/db/connection.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+export const createConnection = () =>{
+    return mongoose.connect(process.env.DB_URL,{
+        maxPoolSize:5
+    });
+}
